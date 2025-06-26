399359f132ff9bf480beed3b9bef531794e25236
Kayla George <kaylatrinitygeorge@gmail.com>
2025-06-26 15:45:18 -0400

    adding header to global layout

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Header } from '@/components/Header'
  
 export default function Document() {
   return (
     <Html>
       <Head />
       <body>
+        <Header></Header>
         <Main />
         <NextScript />
       </body>
