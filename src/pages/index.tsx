dc1b6538d0487f558eab3b5c4c2a8cafb16149b0
Kayla George <kaylatrinitygeorge@gmail.com>
2025-07-03 11:19:46 -0400

    creating signup and hunter login checkbox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,41 @@
-import { Login } from "@/components/Login"
+import Link from "next/link"
+import { useState } from "react"
 
+export default function Login() {
+    const [formData, setFormData] = useState<FormData>({email: "", password: ""})
+    const [loading, setLoading] = useState<boolean>(false)
 
-export default function Home() {
+    interface FormData {
+        email: string
+        password: string
+    }
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = event.target
+        setFormData((prev: FormData) => {
+            let newData = {
+                ...prev,
+                [name]: value
+            }
+            return newData
+        })
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        setLoading(true)
+    }
 
     return (
         <div>
-            <Login/>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Email: &nbsp;<input type="email" onChange={handleChange}></input></label>
+                <label>Password: &nbsp;<input type="password" onChange={handleChange}></input></label>
+                <label><input type="checkbox"></input>I am a hunter</label>
+                <button type="submit">Login</button>
+            </form>
+            <Link href="/signup">Don't have an account? Sign up!</Link>
         </div>
     )
 }
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link"
 import { useState } from "react"
 
-export const Login = () => {
-    const [formData, setFormData] = useState<FormData>({email: "", password: ""})
+export default function Signup() {
+    const [formData, setFormData] = useState<FormData>({name: "", email: "", password: ""})
     const [loading, setLoading] = useState<boolean>(false)
 
     interface FormData {
+        name: string
         email: string
         password: string
     }
@@ -27,11 +29,14 @@ export const Login = () => {
 
     return (
         <div>
+            <h1>Signup</h1>
             <form onSubmit={handleSubmit}>
+                <label>Name: &nbsp;<input type="text" onChange={handleChange}></input></label>
                 <label>Email: &nbsp;<input type="email" onChange={handleChange}></input></label>
                 <label>Password: &nbsp;<input type="password" onChange={handleChange}></input></label>
                 <button type="submit">Login</button>
             </form>
+            <Link href="/">Already have an account? Log in.</Link>
         </div>
     )
 }
