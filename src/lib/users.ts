ce7087c3309d6119b57f9d7376b3ed739d3e7554
Kayla George <kaylatrinitygeorge@gmail.com>
2025-07-12 23:38:27 -0400

    user lib methods changed to accomodate for spring backend (next api methods commented out)

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -1,7 +1,8 @@
 import { User } from "@/types/user"
 
-export const fetchUsers = async (): Promise<User[]> => {
-    const url = `/api/users`
+export const fetchUsers = async (): Promise<Omit<User, "password">[]> => {
+    // const url = `/api/users`
+    const url = `http://localhost:8080/api/users`
     const response = await fetch(url, {
         method: "GET"
     })
@@ -12,40 +13,33 @@ export const fetchUsers = async (): Promise<User[]> => {
     return data
 }
 
-export const fetchUserByEmail = async (email: string): Promise<User | undefined> => {
-    const users = await fetchUsers()
-    return users.find(user => user.email === email)
+export const fetchUserById = async (id: number): Promise<Omit<User, "password"> | undefined> => {
+    const url = `http://localhost:8080/users/${id}`
+    const response = await fetch(url, {
+        method: "GET"
+    })
+    if (response.status === 404) {throw new Error(`User not found`)}
+    if (!response.ok) {throw new Error(`Failed to fetch user`)}
+    const data: Omit<User, "password"> = await response.json()
+    return data
+
 }
 
-export const fetchUserById = async (id: number): Promise<User | undefined> => {
-    const users = await fetchUsers()
-    return users.find(user => user.id === id)
+export const authenticateUser = async (user: {email: string, password: string, is_hunter: boolean}): Promise<Omit<User, "password"> | undefined> => {
+    const url = `http://localhost8080/auth/login?email=${user.email}&password=${user.password}&is_hunter=${user.is_hunter}`
+    const response = await fetch(url, {
+        method: "GET"
+    })
+    if (response.status === 404) {throw new Error(`Invalid email`)}
+    if (response.status === 401) {throw new Error(`Invalid password`)}
+    if (response.status === 403) {throw new Error(`Invalid hunter status`)}
+    if (!response.ok) {throw new Error(`Failed to authenticate user`)}
+    const data: Omit<User, "password"> = await response.json()
+    return data
 }
 
-export const authenticateUser = async (user: {email: string, password: string, is_hunter: boolean}) => {
-    const existingUser = await fetchUserByEmail(user.email)
-    if (!existingUser) {
-        throw new Error(`Invalid email`)
-    }
-    if (existingUser.password !== user.password) {
-        throw new Error(`Invalid password`)
-    }
-    if (existingUser.is_hunter && !user.is_hunter) {
-        throw new Error(`Confirm hunter status`)
-    }
-    if (!existingUser.is_hunter && user.is_hunter) {
-        throw new Error(`Hunter status rejected`)
-    }
-    const {password, ...publicUserData} = existingUser
-    return publicUserData
-}
-
-export const fetchCreateUser = async (newUser: Omit<User,"id">): Promise<User> => {
-    const existingUser = await fetchUserByEmail(newUser.email)
-    if (existingUser) {
-        throw new Error(`A user with that email already exists`)
-    }
-    const url = `api/users`
+export const fetchCreateUser = async (newUser: Omit<User,"id">): Promise<Omit<User, "password"> | undefined> => {
+    const url = `http://localhost:8080/api/users`
     const response = await fetch(url, {
         method: "POST",
         headers: {'Content-Type': 'application/json'},
@@ -57,3 +51,49 @@ export const fetchCreateUser = async (newUser: Omit<User,"id">): Promise<User> =
     const data: User = await response.json()
     return data
 }
+
+// export const fetchUserById = async (id: number): Promise<User | undefined> => {
+//     const users = await fetchUsers()
+//     return users.find(user => user.id === id)
+// }
+
+// export const fetchUserByEmail = async (email: string): Promise<User | undefined> => {
+//     const users = await fetchUsers()
+//     return users.find(user => user.email === email)
+// }
+
+// export const authenticateUser = async (user: {email: string, password: string, is_hunter: boolean}) => {
+//     const existingUser = await fetchUserByEmail(user.email)
+//     if (!existingUser) {
+//         throw new Error(`Invalid email`)
+//     }
+//     if (existingUser.password !== user.password) {
+//         throw new Error(`Invalid password`)
+//     }
+//     if (existingUser.is_hunter && !user.is_hunter) {
+//         throw new Error(`Confirm hunter status`)
+//     }
+//     if (!existingUser.is_hunter && user.is_hunter) {
+//         throw new Error(`Hunter status rejected`)
+//     }
+//     const {password, ...publicUserData} = existingUser
+//     return publicUserData
+// }
+
+// export const fetchCreateUser = async (newUser: Omit<User,"id">): Promise<User> => {
+//     const existingUser = await fetchUserByEmail(newUser.email)
+//     if (existingUser) {
+//         throw new Error(`A user with that email already exists`)
+//     }
+//     const url = `api/users`
+//     const response = await fetch(url, {
+//         method: "POST",
+//         headers: {'Content-Type': 'application/json'},
+//         body: JSON.stringify(newUser)
+//     })
+//     if (!response.ok) {
+//         throw new Error(`Failed to create user`)
+//     }
+//     const data: User = await response.json()
+//     return data
+// }
