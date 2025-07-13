0b121f8afa97da7c79a6aebde4614026a2ce9970
Kayla George <kaylatrinitygeorge@gmail.com>
2025-07-12 23:31:16 -0400

    commenting out db.ts for spring backend

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,17 @@
-import { Pool } from 'pg';
+// import { Pool } from 'pg';
 
-const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: 'postgres',
-  database: 'the-hunters-journal', // match your Docker/Postgres db name
-});
+// const pool = new Pool({
+//   host: 'localhost',
+//   port: 5432,
+//   user: 'postgres',
+//   password: 'postgres',
+//   database: 'the-hunters-journal', // match your Docker/Postgres db name
+// });
 
-export async function query<T = any>(
-  text: string,
-  params?: any[]
-): Promise<{ rows: T[] }> {
-  const result = await pool.query(text, params);
-  return { rows: result.rows };
-}
+// export async function query<T = any>(
+//   text: string,
+//   params?: any[]
+// ): Promise<{ rows: T[] }> {
+//   const result = await pool.query(text, params);
+//   return { rows: result.rows };
+// }
