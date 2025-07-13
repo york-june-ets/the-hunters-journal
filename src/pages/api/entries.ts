79ef803a75013ed25e012a11954995840717ea30
Kayla George <kaylatrinitygeorge@gmail.com>
2025-07-12 23:35:28 -0400

    commenting out api pages because of db.ts dependency

--- src/pages/api/entries.ts
+++ src/pages/api/entries.ts
@@ -1,22 +1,22 @@
-import { query } from "@/lib/db"
-import { slugify } from "@/lib/entries"
-import { Entry } from "@/types/entry"
-import { NextApiRequest, NextApiResponse } from "next"
+// import { query } from "@/lib/db"
+// import { slugify } from "@/lib/entries"
+// import { Entry } from "@/types/entry"
+// import { NextApiRequest, NextApiResponse } from "next"
 
-export const getEntries = async () => {
-    const {rows} = await query('SELECT * FROM entries ORDER BY id DESC')
-    return rows
-}
+// export const getEntries = async () => {
+//     const {rows} = await query('SELECT * FROM entries ORDER BY id DESC')
+//     return rows
+// }
 
-export const getEntryBySlug = async (slug: string) => {
-    const entries = await getEntries()
-    return entries.find((entry: Entry) => slugify(entry.title) === slug)
-}
+// export const getEntryBySlug = async (slug: string) => {
+//     const entries = await getEntries()
+//     return entries.find((entry: Entry) => slugify(entry.title) === slug)
+// }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    //GET
-    if (req.method === 'GET') {
-        const entries = await getEntries()
-        return res.status(200).json(entries)
-    }
-}
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//     //GET
+//     if (req.method === 'GET') {
+//         const entries = await getEntries()
+//         return res.status(200).json(entries)
+//     }
+// }
--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,30 +1,30 @@
-import { query } from "@/lib/db"
-import { User } from "@/types/user"
-import { NextApiRequest, NextApiResponse } from "next"
+// import { query } from "@/lib/db"
+// import { User } from "@/types/user"
+// import { NextApiRequest, NextApiResponse } from "next"
 
-export const getUsers = async () => {
-    const {rows} = await query('SELECT * FROM users ORDER BY id DESC')
-    return rows
-}
+// export const getUsers = async () => {
+//     const {rows} = await query('SELECT * FROM users ORDER BY id DESC')
+//     return rows
+// }
 
-const createUser = async (user: Omit<User, 'id'>): Promise<User> => {
-    const {rows} = await query(
-        'INSERT into users (name, email, password, is_hunter) VALUES ($1, $2, $3, $4)',
-        [user.name, user.email, user.password, user.is_hunter]
-    )
-    return rows[0]
-}
+// const createUser = async (user: Omit<User, 'id'>): Promise<User> => {
+//     const {rows} = await query(
+//         'INSERT into users (name, email, password, is_hunter) VALUES ($1, $2, $3, $4)',
+//         [user.name, user.email, user.password, user.is_hunter]
+//     )
+//     return rows[0]
+// }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
-        const users = await getUsers()
-        return res.status(200).json(users)
-    } else if (req.method === 'POST') {
-        const {name, email, password, is_hunter} = req.body
-        const newUser = await createUser({name, email, password, is_hunter})
-        return res.status(201).json(newUser)
-    } else {
-        res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE'])
-        res.status(405).end(`Method ${req.method} Not Allowed`)
-    }
-}
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//     if (req.method === 'GET') {
+//         const users = await getUsers()
+//         return res.status(200).json(users)
+//     } else if (req.method === 'POST') {
+//         const {name, email, password, is_hunter} = req.body
+//         const newUser = await createUser({name, email, password, is_hunter})
+//         return res.status(201).json(newUser)
+//     } else {
+//         res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE'])
+//         res.status(405).end(`Method ${req.method} Not Allowed`)
+//     }
+// }
